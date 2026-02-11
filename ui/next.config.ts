12164b4a14d473d37ba12c27d7c471dfeae40657
mizhm <minhnt141204@gmail.com>
2026-02-11 22:26:11 +0700

    ci: optime next build

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -2,14 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${process.env.INTERNAL_API_URL || "http://localhost:8080"}/api/:path*`,
-      },
-    ];
-  },
+  output: "standalone",
 };
 
 export default nextConfig;
